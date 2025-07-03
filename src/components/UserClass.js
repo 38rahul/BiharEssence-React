dd7fb69d9d281e1d73ac9d478b646b08271491fc
rahul-kumar@siemens.com <r.rajput.tiu97gmail.com>
2025-07-03 19:17:18 +0530

    Lecture 08 -  Let's Get Classy

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,25 +8,51 @@ class UserClass extends React.Component {
         // use state in Class Based component
 
         this.state = {
-            count: 12,
-            
-        };
+            userInfo:{
+                name: "Dummy",
+                location: "Default"
+            }
+         };
+
+         //console.log( this.props.name1 +" Child constructor");
     }
+
+     componentDidCatch(){
+         //   console.log("Child componentDidCatch");
+        }
+
+       async componentDidMount(){
+          //  console.log(this.props.name1+ " Child Component Did mount");
+
+          const data = await fetch ("https://api.github.com/user/devrahul97");
+
+          const json = await data.json();
+          console.log(json);
+
+          this.setState({
+            userInfo: json,
+          } )
+          
+        }
+
+        componentDidUpdate(){
+         //   console.log(this.props.name1 +"Child Component Did Update");
+        }
+
+        componentWillUnmount(){
+
+        }
     render() {
-        const {name1, location} = this.props;
-        const {count} = this.state;
+        
+            const {name, company, followers, following} = this.state.userInfo;
          return (
         <div className="user-card">
-            <h1>count: {count}</h1>
-            <button onClick={
-                () =>{
-                    // Never Update State Variables Directly
-                    // this.state.count = this.state.count++; 
-                    // 40 min
-                }
-            }>count Increase</button>
-            <h2>Name: {name1 }</h2>
-            <h2>Locan: {location}</h2>
+        
+            <h2>Name: {name }</h2>
+            <h2>company: {company}</h2>
+            <h2>followers count: {followers}</h2>
+            <h2>following count: {following}</h2>
+          
             <h4>Name: github/@38rahul</h4>
             
         </div>
